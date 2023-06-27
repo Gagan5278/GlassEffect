be767e3b75fc89a0eb2028287cb5f5a39f911f09
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-06-27 15:22:57 +0200

    Initial Commit

---
+++ GlassEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlassEffect
+//
+//  Created by Gagan Vishal  on 2023/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlassEffect/GlassEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassEffectApp.swift
+//  GlassEffect
+//
+//  Created by Gagan Vishal  on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct GlassEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
